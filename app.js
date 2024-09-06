604a6926fc8cf0ce40b5bb144a1b92d742e1cab9
Paulo Mattos <113618531+PauloVMattos@users.noreply.github.com>
2024-09-06 14:40:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa") 
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Personagem Não Encontrado.</p>"
+        return
+    }
+
+        campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados ="";
+    let titulo="";
+    let descricao="";
+    
+    for (let dado of dados){
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultados +=`
+        <div class="item-resultado">
+            <h2><a href=${dado.link} target="_blank">${dado.titulo}</a></h2> 
+                <p class="descricao-meta">${dado.descricao}</p> 
+        </div>
+    `
+        }
+    
+        if(!resultados){
+            resultados = "<p>Nada foi Encontrado</p>"
+        }
+
+    }
+    section.innerHTML=resultados
+}
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados=[
+    {
+        titulo: "Monkey D. Luffy",
+        descricao: "O Pirata de Chapéu de Palha é o protagonista carismático e aventureiro do anime e mangá One Piece. Sonhando em se tornar o Rei dos Piratas, Luffy embarca em uma jornada épica pelos mares, reunindo uma tripulação diversa e leal a bordo do navio Thousand Sunny.",
+        link : "tthps://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy"
+    }, 
+    {
+        titulo: "Ichigo Kurosaki",
+        descricao: "Um adolescente que, após um encontro com uma Shinigami, ganha a habilidade de ver espíritos e se torna um Shinigami substituto.",
+        link : "https://bleach.fandom.com/pt/wiki/Ichigo_Kurosaki"
+    }, 
+    {
+        titulo: "Naruto Uzumaki",
+        descricao: "Um ninja adolescente que sonha em se tornar Hokage, o líder da Vila da Folha. Naruto é conhecido por sua determinação e seu jutsus baseados no elemento vento.",
+        link : "https://naruto.fandom.com/pt/wiki/Naruto_Uzumaki" 
+    }
+];
